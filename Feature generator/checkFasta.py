5035f11871f477436f119b1f7e71781882c08986
GGCL7 <113498910+GGCL7@users.noreply.github.com>
2024-01-02 11:33:44 +0800

    Add files via upload

---
+++ Feature generator/checkFasta.py
@@ -0,0 +1,30 @@
+# -*-codeing = utf-8 -*-
+# @Time : 2022/11/16 09:28
+# @Author : Jiahui Guan
+# @File : checkFasta.py
+# @Software: PyCharm
+import re
+
+def checkFasta(fastas):
+	status = True
+	lenList = set()
+	for i in fastas:
+		lenList.add(len(i[1]))
+	if len(lenList) == 1:
+		return True
+	else:
+		return False
+
+def minSequenceLength(fastas):
+	minLen = 10000
+	for i in fastas:
+		if minLen > len(i[1]):
+			minLen = len(i[1])
+	return minLen
+
+def minSequenceLengthWithNormalAA(fastas):
+	minLen = 10000
+	for i in fastas:
+		if minLen > len(re.sub('-', '', i[1])):
+			minLen = len(re.sub('-', '', i[1]))
+	return
